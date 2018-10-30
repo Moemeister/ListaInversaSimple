844eff113da9c6f5986e11f1cb415084ca7c95f6
MacbookPro <macbookpro@MacbookPros-MacBook-Pro.local>
2018-10-30 17:16:05 -0600

    Complete but idk why :'v

--- Lista inversa.c
+++ Lista inversa.c
@@ -6,19 +6,48 @@ typedef struct Nodo{
     int valor;
     struct Nodo* sig;
 }Nodo;
+Nodo* L = NULL;
+Nodo* createNodo(){
+    Nodo* n = (Nodo*)malloc(sizeof(Nodo));
+    return n;
+}
+Nodo* createList(){
+    return NULL;
+}
+int generateRamdomNumber(){
+    return rand()%1000;
+}
 
-
-
-//GLOBAL VARs
-
-
-
-
-
-
-
+void insert(Nodo* n){
+    n->sig = L;
+    L=n;
+}
+void llenar(int tam){
+    Nodo* nuevo = NULL;
+    int arr[tam];
+    for(int i = 0; i <= tam; i++){
+        arr[i] = generateRamdomNumber();
+        printf("%d\n",arr[i]);
+        Nodo* nuevo = createNodo();
+        nuevo->valor = arr[i];
+        nuevo->sig=NULL;
+        insert(nuevo);
+    }
+}
+void show(){
+    Nodo* otroL = L;
+    printf("LISTA INVERTIDA: \n");
+    while(otroL!=NULL){
+        printf("%d\n",otroL->valor);
+        otroL = otroL->sig;
+    }
+}
 
 int main(){
-
+    time_t t;
+    srand((unsigned) time(&t));
+    L= createList();
+    llenar(7);
+    show();
     return 0;
 }
