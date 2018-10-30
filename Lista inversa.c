618906a8e35eaf0814d8d06e16432b79609e3fae
MacbookPro <macbookpro@MacbookPros-MacBook-Pro.local>
2018-10-30 16:25:34 -0600

    Initial

---
+++ Lista inversa.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+//Struct Node
+typedef struct Nodo{
+    int valor;
+    struct Nodo* sig;
+}Nodo;
+
+
+
+//GLOBAL VARs
+
+
+
+
+
+
+
+
+int main(){
+
+    return 0;
+}
